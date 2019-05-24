3f86a47456cec12099ab5749f43de0c1cbac525e
Joao Veras <joaohg.veras@gmail.com>
2019-05-24 01:52:22 -0300

    adding cors

--- api/app/server.js
+++ api/app/server.js
@@ -1,14 +1,22 @@
 const restify = require('restify');
 const albumHandlers = require('./handlers/album');
+const corsMiddleware = require('restify-cors-middleware');
+
+const cors = corsMiddleware({
+  preflightMaxAge: 5, //Optional
+  origins: ['*']
+});
 
 const server = restify.createServer({
   name: 'pd',
   version: '1.0.0'
 });
 
+server.pre(cors.preflight);
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
+server.use(cors.actual);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
